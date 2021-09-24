87ff982c7a8655226d3a2770256a7977f0b8f836
Alix17 <wildflowerinabox@gmail.com>
2021-09-23 23:58:48 -0700

    Revising as I google

--- script.js
+++ script.js
@@ -1,69 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var characters = "Characters";
+var lettersup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var letterslow = "abcdefghijklmnopqrstuvwxyz";
+var numerical = "0123456789";
+var special = "!@#$%^&*";//Shift 1-8 are specials
+var chooseLength 
+console.log (characters)
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);(){
-  
-let passwordLength = prompt (alert.7)
-//is this where I list my var?//password variables //global variables
-var characters = document.querySelector("Characters");
-var lettersup = document.querySelector("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var letterslow = document.querySelector("abcdefghijklmnopqrstuvwxyz");
-var numerical = document.querySelector("0123456789");
-var special = document.querySelector("!@#$%^&*");//Shift 1-8 are specials
+generateBtn.addEventListener("click", writePassword);
 
-//betwween 8-128
-if(chars <8) {
-  alert1("Needs more Characters");
-}else if (chars >128){
-  alert2 ("Too many Characters");
-} else //do I need an else statement? if number between 8-128
-{
-  alert3 ("Do you want uppercase letters?");}//how to make it go to uppercase?
 
-  var lettersup "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  if confirm.alert3 ==true; {
-    lettersup 
+prompt ("Please choose a number between 8 and 128 characters");
+if (characterCount < 8 || characterCount > 128){
+chooseLength
+};
+
+ if confirm ("Do you want uppercase letters?")
+   ==true; {
+    lettersup = lettersup.concat
+    //lettersup.concat? or something else
+  } 
+  //console.log lettersup.concat??
+  if letterslow = prompt alert4
+  if (confirm.alert4 ==true;) {
+    letterslow = letterslow.concat
   }
-//uppercase
-confirm alert3
+  if numerical = prompt alert5
+  if (confirm.alert5 ==true;) {
+    numerical = numerical.concat
+    }
+  if special = prompt alert6
+  if (confirm.alert6 ==true;) {
+    special = special.concay
+  }
 
-//lowercase
-confirm alert4
 
-//special
-confirm alert5
-
-//numbers
-confirm alert6
-
-//alerts?
-alert7.textContent="Please choose a number between 8 and 128 characters"
 alert1.textContent="Needs more Characters";
 alert2.textContent="Too Many Characters";
 alert3.textContent="Do you want uppercase letters?";
 alert4.textContent="Do you want lowercase letters?";
 alert5.textContent="Do you want numbers?";
 alert6.textContent="Do you want special characters?";
+//is this where I list my var? does it need to be above?
 
-function generate() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function writePassword() {
+  var length = 8,
+      charset = //?????
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) { ////charset
+      retVal += charset.charAt(Math.floor(Math.random() * n)); ///charset
+  }
+  return retVal; 
 }
 
-//code snippet from website
-if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
-  alert("please enter a valid tag");
-} else {
-  // Creates element based on tag entered by user
-  //We use the method createElement() to create new elements, as follows:
-  var tag = document.createElement(tagName);
+  var generateBtn = document.querySelector("generate");
 
-  var completePassword = ' ';
-  var elements = [
-    characters, lettersup, letterslow, numerical, special
-  ];
-  elements.forEach
+
+generateBtn.addEventListener('click', function(newPassword){
+    console.log (generateBtn);
+});
