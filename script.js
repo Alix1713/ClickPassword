a8e02227b8bb1bbda911ce30a551c13c843ab185
Alix17 <wildflowerinabox@gmail.com>
2021-09-24 10:13:44 -0700

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 var characters = "Characters";
 var lettersup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var letterslow = "abcdefghijklmnopqrstuvwxyz";
+var lettersLow = "abcdefghijklmnopqrstuvwxyz";
 var numerical = "0123456789";
 var special = "!@#$%^&*";//Shift 1-8 are specials
 var chooseLength
@@ -13,42 +13,29 @@ console.log (characters)
 generateBtn.addEventListener("click", writePassword);
 
 
-prompt ("Please choose a number between 8 and 128 characters");
-if (chooseLength < 8 || chooseLength > 128){
-chooseLength = 5
+chooseLength = prompt ("Please choose a number between 8 and 128 characters");
+if (chooseLength > 8 || chooseLength < 128){
+alert ("Needs More Characters/Too many characters")
 };
-
- if confirm ("Do you want uppercase letters?")
-   ==true; {
-    lettersup = lettersup.concat
-    //lettersup.concat? or something else
-  } 
-  //console.log lettersup.concat??
-  if letterslow = prompt alert4
-  if (confirm.alert4 ==true;) {
-    letterslow = letterslow.concat
+console.log (promptone)
+ lettersup = prompt ("Do you want uppercase letters?");
+   (lettersup==true); {
+  
+lettersLow = prompt ("Do you want lowercase letters?");
+  (confirm lettersLow==true); {
+    
   }
-  if numerical = prompt alert5
-  if (confirm.alert5 ==true;) {
-    numerical = numerical.concat
+numerical = prompt ("Do you want numbers?");
+   (confirm numerical==true); {
+  
     }
-  if special = prompt alert6
-  if (confirm.alert6 ==true;) {
-    special = special.concay
+special = prompt ("Do you want special characters?");
+  (confirm special ==true); {
   }
-
-
-alert1.textContent="Needs more Characters";
-alert2.textContent="Too Many Characters";
-alert3.textContent="Do you want uppercase letters?";
-alert4.textContent="Do you want lowercase letters?";
-alert5.textContent="Do you want numbers?";
-alert6.textContent="Do you want special characters?";
-//is this where I list my var? does it need to be above?
 
 function writePassword() {
-  var length = 8,
-      charset = //?????
+  var length = chooseLength
+      charset = genList
       retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) { ////charset
       retVal += charset.charAt(Math.floor(Math.random() * n)); ///charset
