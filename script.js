85b2329ea693db0fabe7a141d4aaabc5d0a75f90
Alix17 <wildflowerinabox@gmail.com>
2021-09-23 20:42:22 -0700

    Framework pt2

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-//no touch
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);(){
+  
+let passwordLength = prompt (alert.7)
+//is this where I list my var?//password variables //global variables
+var characters = document.querySelector("Characters");
+var lettersup = document.querySelector("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var letterslow = document.querySelector("abcdefghijklmnopqrstuvwxyz");
+var numerical = document.querySelector("0123456789");
+var special = document.querySelector("!@#$%^&*");//Shift 1-8 are specials
+
+//betwween 8-128
+if(chars <8) {
+  alert1("Needs more Characters");
+}else if (chars >128){
+  alert2 ("Too many Characters");
+} else //do I need an else statement? if number between 8-128
+{
+  alert3 ("Do you want uppercase letters?");}//how to make it go to uppercase?
+
+  var lettersup "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  if confirm.alert3 ==true; {
+    lettersup 
+  }
+//uppercase
+confirm alert3
+
+//lowercase
+confirm alert4
+
+//special
+confirm alert5
+
+//numbers
+confirm alert6
+
+//alerts?
+alert7.textContent="Please choose a number between 8 and 128 characters"
+alert1.textContent="Needs more Characters";
+alert2.textContent="Too Many Characters";
+alert3.textContent="Do you want uppercase letters?";
+alert4.textContent="Do you want lowercase letters?";
+alert5.textContent="Do you want numbers?";
+alert6.textContent="Do you want special characters?";
+
+function generate() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
-// Button done?
-generateBtn.addEventListener("click", function writePassword);
-//password variables
-var characters="Characters"
-var lettersup ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var letterslow ="abcdefghijklmnopqrstuvwxyz"
-var numerical ="0123456789"
-var special ="!@#$%^&*"
-///??
-let chars = prompt("")
-//Shift 1-8 are specials
+//code snippet from website
+if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
+  alert("please enter a valid tag");
+} else {
+  // Creates element based on tag entered by user
+  //We use the method createElement() to create new elements, as follows:
+  var tag = document.createElement(tagName);
 
-//can this be for the whole variable array?
-//if 
-prompt "How many characters do you Want in your new password?"
-if(chars <8) {
-  console.log("Needs more Characters");
-}else if (chars >128){
-  console.log("Too many Characters")
-} {alert ("Do you want uppercase letters?")}
-
-if(lettersup) //?
-//how to make it go to next?
-
-//if categories
-if (lettersup)
-
-if (letterslow)
-
-if (numerical)
-
-if (special)
-
-//hits head on keyboard repeatedly
+  var completePassword = ' ';
+  var elements = [
+    characters, lettersup, letterslow, numerical, special
+  ];
+  elements.forEach
