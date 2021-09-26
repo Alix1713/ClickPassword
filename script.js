fcb5b7ac0940c3f43ccdd8b21e0a447d9465b662
Alix17 <wildflowerinabox@gmail.com>
2021-09-26 12:53:25 -0700

    Review and changes

--- script.js
+++ script.js
@@ -1,51 +1,67 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+var wantsUppercase //needed variable to catch line 22
+var wantsLowercase
+var wantsNumbers
+var wantsSpecial
 var characters = "Characters";
 var lettersup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lettersLow = "abcdefghijklmnopqrstuvwxyz";
 var numerical = "0123456789";
 var special = "!@#$%^&*";//Shift 1-8 are specials
-var chooseLength
-console.log (characters)
+
+var chooseLength //no brackets since one answer
+var charSet =[] //brackets to create an array to catch items
+console.log(characters)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-chooseLength = prompt ("Please choose a number between 8 and 128 characters");
-if (chooseLength > 8 || chooseLength < 128){
-alert ("Needs More Characters/Too many characters")
-};
-console.log (promptone)
- lettersup = prompt ("Do you want uppercase letters?");
-   (lettersup==true); {
-  
-lettersLow = prompt ("Do you want lowercase letters?");
-  (confirm lettersLow==true); {
-    
+//when click happens this happens V
+chooseLength = prompt("Please choose a number between 8 and 128 characters");
+if (chooseLength < 8 || chooseLength > 128) {
+  alert("Needs More Characters/Too many characters")//needs else
+  } else { // ^ initial check
+    //listening for event from user
+console.log(promptone)
+wantsUppercase = confirm("Do you want uppercase letters?"); // prompt opens a box to enter something
+ {
+  if (wantsUppercase === true) { //no confirm again, use triple equals!!!!
+charSet.push("uppercase")
   }
-numerical = prompt ("Do you want numbers?");
-   (confirm numerical==true); {
-  
+  wantsLowercase = confirm("Do you want lowercase letters?"); //confirm is a yes or no use confirm not prompt
+  {
+    if (wantsLowercase===true){
+      charSet.push("lowercase")
+  }
+  wantsNumbers = confirm("Do you want numbers?");
+  {
+    if (wantsNumbers ===true){
+      charSet.push("numbers")
+  }
+  wantsSpecial = confirm("Do you want special characters?");
+{
+  if (wantsSpecial===true){
+    charSet.push("special")
+}}
+  function writePassword() {
+    var length = chooseLength
+    charSet = charSet
+    retVal = "";
+    for (var i = 0, n = charSet.length; i < length; ++i) { ////charset
+      retVal += charSet.charAt(Math.floor(Math.random() * n)); ///charset
     }
-special = prompt ("Do you want special characters?");
-  (confirm special ==true); {
+    return retVal;
   }
-
-function writePassword() {
-  var length = chooseLength
-      charset = genList
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) { ////charset
-      retVal += charset.charAt(Math.floor(Math.random() * n)); ///charset
-  }
-  return retVal; 
 }
-
   var generateBtn = document.querySelector("generate");
+// ^ has to be above event listener due to "generate"
+// V creates variable "generate"
+  generateBtn.addEventListener('click', function (writePassword) {
+    console.log(generateBtn); //takes in type of event in this case its click
+  } //function is what the event does
 
 
-generateBtn.addEventListener('click', function(newPassword){
-    console.log (generateBtn);
-});
+
+//commment out the basic structure of how the program should run
+//write out every single step
