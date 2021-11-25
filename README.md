d21b659afbd93ab0694f9be5bec1cee2fbf449e0
Alix17 <wildflowerinabox@gmail.com>
2021-11-24 23:54:32 -0800

    created a var to hold the generated password and then display it on page

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ClickPassword
-Thinking about my dads password, which may as well be password, I wanted something easy to click and get a secure password created. 
+# Click Password
+This is a javascript project to generate a randomized password.
+
+## Getting Started
+
+Click the button
+
+# Adding a unique id to each note
+![Code Snippet](snip.png)
+
+
+# Link
+* [Deployed link](https://alix1713.github.io/ClickPassword/)
+* [Github Repo](https://github.com/Alix1713/ClickPassword)
+
+## Built With
+
+* [HTML](https://www.w3schools.com/html/html_intro.asp)
+* [UC Berkeley Extension Program](https://extension.berkeley.edu/)
+
+## Alix Friedman
+
+* [My GitHub](https://www.github.com/Alix1713)
+* [My LinkedIn](https://www.linkedin.com/in/alix1713)
+
+
+## Acknowledgments
+
+Thanks to Jerome for taking the time to go over this with me after class.
--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 //Lines 2 and 4 point to the html element by its id and create a variable to use later on in the code
 var generateBtn = document.getElementById("generate");
 var textArea = document.getElementById("password");
-
-//these are my unique variables intended to be tracked throughout the program
-
-//its a global variable that is a flag pointing to the boolean of wether they want uppercase or not
-//global variables
 var password = "";
 //these are undefined until they are assigned value
 var wantsUppercase;
@@ -18,28 +13,8 @@ var lettersup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lettersLow = "abcdefghijklmnopqrstuvwxyz";
 var numerical = "0123456789";
 var special = "!@#$%^&*"; //Shift 1-8 are specials
-
 var chooseLength; //no brackets since one answer
 var charSet = ""; //brackets to create an array to catch items
-
-// //VERBS --my functions
-// //we need to assign something to happen when we click the button
-// function startPassword() {
-//   //prompt user to choose a number between 8-128
-//   var charCount = prompt("Please choose a number between 1 and 128");
-//   passwordLegenth = parseInt(charCount);
-//   console.log(passwordLegenth);
-//   //assign the number to the variable
-//   //start the next prompt and ask for uppercase letters
-//   //assign the value to that boolean to the variable of WantsUppercase
-//   //start the next prompt of lowercase
-//   //assign the value to wantslowercase
-//   //start the prompt of wants numbers
-//   //assign the value to wantsnumbers
-//   //starts the wants special
-//   //assign the value to wants special
-//   //takes all of the values from the prompts and makes a password
-// };
 
 function startPassword() {
   var chooseLength = prompt(
@@ -48,8 +23,6 @@ function startPassword() {
   if (chooseLength < 8 || chooseLength > 128) {
     alert("Needs More Characters/Too many characters"); //needs else
   } else {
-    // ^ initial check
-    //listening for event from user
     wantsUppercase = confirm("Do you want uppercase letters?"); // prompt opens a box to enter something
     console.log(wantsUppercase);
     if (wantsUppercase === true) {
@@ -70,16 +43,15 @@ function startPassword() {
     }
     retVal = "";
     for (var i = 0, n = charSet.length; i < chooseLength; i++) {
-      ////charset
       retVal += charSet.charAt(Math.floor(Math.random() * n)); ///charset
     }
     var newPass = retVal;
-    // var newPass = generateBtn();
     console.log(newPass);
   }
+  var showPass = document.getElementById("password");
+showPass.value = retVal;
 }
 //newPass using innerhtml
 //display a generated password
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", startPassword);
